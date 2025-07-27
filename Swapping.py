4409186d41826442e9ff3a72a159d3133ff99252
sunjaygir08 <sunjaygir@gmail.com>
2025-07-27 11:44:48 +0500

    swapping Concepts

--- Swapping.py
+++ Swapping.py
@@ -1,3 +1,4 @@
+#swapping Concept
 #Swapping using temp Vraiable
 a=100
 b=150
@@ -18,6 +19,19 @@ print("Before swapping:")
 print("a =", a)
 print("b =", b)
 a,b=b,a
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+#swapping using arithmetic operations
+print("Using arithmetic operations")
+a = 100
+b = 150
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+a = a + b       #a = 100+150 = 250
+b = a - b       #b = 250-150 = 100
+a = a - b       #a = 250-100 = 150
 print("After swapping:")
 print("a =", a)
 print("b =", b)
