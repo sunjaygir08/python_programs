b512858df00c61db2013381e31c2b3a9840bcf5d
sunjaygir08 <sunjaygir@gmail.com>
2026-01-31 15:23:24 +0500

    Fix sorting issue in brother names list

---
+++ Core CS/Exercise program1.py
@@ -0,0 +1,13 @@
+# This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.
+
+# print("Simeoni")
+# print("Juhani")
+# print("Eero")
+# print("Lauri")
+# print("Aapo")
+# print("Tuomas")
+# print("Timo")
+
+Seven_brothers=["Simeoni","Juhani","Eero","Lauri","Aapo","Tuomas","Timo"]
+for names in sorted(Seven_brothers):
+    print(names)
